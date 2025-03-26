35bf730fac7a5d5e1d3f7d54c22ebd55a0894d82
Daryl Hornick-Becker <darylhornickbecker@gmail.com>
2025-03-26 16:10:47 -0400

    Styling

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,32 @@ function App() {
 
 const [puppies, setPuppies] = useState(puppyList)
 console.log(puppies)
+const [featPupId, setFeatPupId] = useState(null)
+
+const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+console.log(featuredPup)
 
   return (
+    
       <div className="App">
         { 
           puppies.map((puppy) => {
-            return <p>{puppy.name}</p>
+            return <p className="puppy" onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
           })
         }
+              {featPupId && (
+        <div className="featured">
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+        
       </div>
+
+      
   
   )
 }
